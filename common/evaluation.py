4b28703c93c019c93b9c68956314fa7dcdc584f3
felihong <echozhu1991@gmail.com>
2022-08-24 14:00:15 +0200

    Update evaluation.py

--- common/evaluation.py
+++ common/evaluation.py
@@ -1,14 +1,15 @@
+
 import os
 import numpy as np
-import nibabel as nib
 import SimpleITK as sitk 
+import nibabel as nib
 from skimage.measure import compare_ssim
+import pandas as pd
+pd.options.mode.chained_assignment = None
 import radiomics
 radiomics.logger.setLevel('ERROR')
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
-import pandas as pd
-pd.options.mode.chained_assignment = None
 
 
 def compute_nrmse(real, pred):
@@ -29,9 +30,7 @@ def compute_psnr(real, pred):
 
 
 def compute_ssim(real, pred):
-    # Normalize both inputs
-    real_norm, pred_norm = real / float(np.max(real)), pred / float(np.max(pred))
-    ssim = compare_ssim(real_norm, pred_norm)
+    ssim = compare_ssim(real/ float(np.max(real)), pred/ float(np.max(pred)))
     return ssim
 
 
@@ -56,30 +55,36 @@ def percentage_error(real, pred):
     return abs(pred - real) / abs(real) * 100
 
 
-class PETSubjectEvaluation:
+class Evaluator():
     def __init__(
         self,
-        real_dir,
-        gen_dir, 
-        mask_dir,
-        meta_file,
-        report_csv=None,
-        num_subject=50
+        # Directory containing all original fulldose PETs.
+        real_dir='./ground-truth',
+        # Directory containing reconstructed PETs.
+        pred_dir='./test',
+        # Directory containing all masks with organ labels.
+        mask_dir='./mask',
+        # Path of metadata csv file.
+        meta_info='./meta_info.csv',
+        # Path of evaluation output, if not exist a directory with the name will be created.
+        output_path='./output'
     ):
         self.real_dir = real_dir
-        self.pred_dir = gen_dir
+        self.pred_dir = pred_dir
         self.mask_dir = mask_dir
-        self.report_csv = report_csv
-        self.num_subject = num_subject
-        self.meta_df = pd.read_csv(meta_file)  # Extract subjects' meta data
+        self.meta_info = meta_info
+        self.output_path = output_path
+        if not os.path.exists(self.output_path):
+            os.makedirs(self.output_path)
+        self.report_csv = os.path.join(self.output_path, 'report.csv')
         self.selected_feature_list = [
-			'firstorder_RootMeanSquared',
-			'firstorder_90Percentile',
-			'firstorder_Median',
-			'glrlm_HighGrayLevelRunEmphasis', 
-			'glszm_ZonePercentage',
-			'glcm_JointAverage'
-		]
+            'firstorder_RootMeanSquared',
+            'firstorder_90Percentile',
+            'firstorder_Median',
+            'glrlm_HighGrayLevelRunEmphasis', 
+            'glszm_ZonePercentage',
+            'glcm_JointAverage'
+        ]
         self.SUV_feature_list = ['SUV_mean', 'SUV_max', 'PSNR', 'TotalLesionMetabolism'] 
         self.organ_list = ['liver', 'heart', 'kidneyRight', 'kidneyLeft']
         self.extractor = radiomics.featureextractor.RadiomicsFeatureExtractor(binCount=100)
@@ -92,30 +97,27 @@ class PETSubjectEvaluation:
         """
         pred= nib.load(os.path.join(self.pred_dir, subject_file) ).get_fdata()
         real = nib.load(os.path.join(self.real_dir, subject_file)).get_fdata()
-        subject_pair = np.stack((real, pred), axis=0)
-        return subject_pair
+        mask = nib.load(os.path.join(self.mask_dir, subject_file)).get_fdata()
+        return pred, real, mask
 
     def _get_metainfo(self, subject:str):
         """
-        Extract patient meta info from meta data dataframe.
-        :param subject: Subject name.
-        :return: Dictionay containing subject meta information.
+        Extract subject metadata.
+        :param subject: Subject pid.
         """
-        ds = self.meta_df.loc[self.meta_df['pid'] == subject]
+        df = pd.read_csv(self.meta_info)
+        subject_serie = df[df['PID'] == subject]
+
         return {
-            'gender': str(ds['gender']),
-            'age': int(ds['age']),
-            'weight': float(ds['weight']),
-            'manufacturer_model': str(ds['manufacturer_model']),
-            'dose': float(ds['dose']),
-            'radionuclide_name': str(ds['radionuclide_name'])
+            'weight': subject_serie['weight'].values[0],
+            'dose': subject_serie['dose'].values[0]
         }
 
     def _get_local_MAPE(self, row): 
         """
         Compute weighted MAPE of all local feature evaluation values.
         :param row: Dataframe row containing all local feature evaluation of a subject.
-        :return: Weighted MAPE value.
+        :return: Weighted MAPE value
         """
         sum = 0
         feature_ls = self.SUV_feature_list + self.selected_feature_list
@@ -131,23 +133,22 @@ class PETSubjectEvaluation:
 
     def _get_subject_SCORE(self, row):
         """
-        Compute weighted score based on global (NRMSE, PSNR, SSIM) metrics and local MAPE metric.
-        Using positive values of PSNR and SSIM, negative values of NRMSE and MAPE error.
-        :param row: Dataframe row containing all four global and local features.
-        :return: Weighted score value.
+        Compute weighted score based on global (NRMSE, PSNR, SSIM) metrics and local MAPE metric
+        using positive values of PSNR and SSIM, negative values of NRMSE and MAPE error.
+        :param row: Dataframe row containing all four global and local features
+        :return: Weighted score value
         """
         global_score = 0.4 * row['PSNR'] + 0.2 * row['SSIM'] - 0.4 * row['NRMSE']
         local_score = - row['MAPE']
-        return 0.5 * global_score + 0.5 * local_score
+        return (0.5 * global_score + 0.5 * local_score) 
 
     def _evaluate_single_global(self, subject_file:str):
         """
-        Compute single subject NRMSE, PSNR and SSIM metrics.
-        :param subject_file: Subject nifti filename.
-        :return: Dataframe evaluting global nrmse, psnr, ssim metrics of one subject.
+        Compute single subject NRMSE, PSNR and SSIM metrics
+        :param subject_file: Subject nifti filename
+        :return: Dataframe evaluting global nrmse, psnr, ssim metrics of one subject
         """
-        pair = self._load_data(subject_file)
-        sub_real, sub_pred = pair[0], pair[-1]
+        sub_pred, sub_real, _ = self._load_data(subject_file)
         return pd.DataFrame({
             'PID': [subject_file.split(os.extsep)[0]],
             'NRMSE': [compute_nrmse(real=sub_real, pred=sub_pred)], 
@@ -157,24 +158,20 @@ class PETSubjectEvaluation:
 
     def _evaluate_single_local(self, subject_file:str):
         """
-        Compute single subject SUV, selected local feature metrics.
-        :param subject_file: Subject nifti filename.
-        :return: Dataframe evaluating local SUV and selected features.
+        Compute single subject SUV, selected local feature metrics
+        :param subject_file: Subject nifti filename
+        :return: Dataframw evaluating local SUV and selected features
         """
         # Find the subject's meta information
         subject = subject_file.split(os.extsep)[0]
         meta = self._get_metainfo(subject)
         SUV_ratio = meta['weight'] * 1000 / meta['dose']
 
-        # Load nifti files and check affine
-        gen = nib.load(os.path.join(self.pred_dir, subject_file))
-        original = nib.load(os.path.join(self.real_dir, subject_file))
-        mask = nib.load(os.path.join(self.mask_dir, subject_file))
+        gen, original, sphere = self._load_data(subject_file)
 
-        # Generate fulldose, prediction and mask array
-        sphere = np.array(mask.dataobj) 
-        gen_ary = np.array(gen.dataobj) * SUV_ratio
-        original_ary = np.array(original.dataobj) * SUV_ratio
+        # Generate fulldose, prediction array
+        original_ary = original * SUV_ratio
+        gen_ary = gen * SUV_ratio
         original_sitk = sitk.GetImageFromArray(original_ary)
         gen_sitk = sitk.GetImageFromArray(gen_ary)
 
@@ -213,10 +210,10 @@ class PETSubjectEvaluation:
 
     def compute_global_metrics(self):
         """
-        Evaluation global NRMSE, PSNR, SSIM metrics for all predicted (model generated) subjects.
-        :return: Dataframe containing NRMSE, PSNR, SSIM metric evaluation results.
+        Evaluation global NRMSE, PSNR, SSIM metrics for all predicted (model generated) subjects
+        :return: Dataframe containing NRMSE, PSNR, SSIM metric evaluation results
         """
-        assert len(os.listdir(self.pred_dir)) == self.num_subject, print("Mismatch subject amount")
+        assert len(os.listdir(self.real_dir)) == len(os.listdir(self.pred_dir)) == self.num_subject, print("Mismatch subject amount")
 
         global_df = pd.DataFrame()
         for subject_file in os.listdir(self.pred_dir):
@@ -227,10 +224,10 @@ class PETSubjectEvaluation:
 
     def compute_local_metrics(self, verbose=False, organ_verbose=False):
         """
-        Evaluate local SUV, selected metrics for all subjects.
-        :param verbose: Boolean value specifying whether return all local features.
-        :param organ_verbose: Boolean value specifying whether return local percentage error averaged across all organs.
-        :return: Dataframe containing all SUV, selected feature metric evaluation results.
+        Evaluate local SUV, selected metrics for all subjects
+        :param verbose: Boolean value specifying whether return all local features
+        :param organ_verbose: Boolean value specifying whether return local percentage error averaged across all organs
+        :return: Dataframe containing all SUV, selected feature metric evaluation results
         """
         local_df = pd.DataFrame()
         for subject_file in os.listdir(self.pred_dir):
@@ -245,35 +242,25 @@ class PETSubjectEvaluation:
             local_df = local_df[[col for col in local_df.columns if 'percentage_error' in col or col=='Organ' or col=='PID']]
             local_df['MAPE'] = local_df.apply(lambda row: self._get_local_MAPE(row), axis=1)
             return local_df.groupby('PID').agg('mean')[['MAPE']]
-
-    def evaluate_all(self, save_to_file=True, local_verbose=False):
+        
+    def evaluate_all(self):
         """
-        Evaluate global and local metrics of all subjects.
-        :param save_to_file: Boolean value specifying whether save evaluation report to csv.
-        :return: Dataframe of evaluation result containing [global_NRMSE, global_PSNR, global_SSIM, local_MAPE].
+        Evaluate global and local metrics of all subjects
+        : param save_to_file: Boolean value specifying whether save evaluation report to csv
+        :return: Dataframe of evaluation result containing [global_NRMSE, global_PSNR, global_SSIM, local_MAPE]
         """
         all_df = pd.DataFrame()
-        for subject_file in os.listdir(self.pred_dir):
+        for subject_file in [x for x in os.listdir(self.pred_dir) if not x.startswith('.')]:
             print('Evaluating subject ' + subject_file)
             global_df = self._evaluate_single_global(subject_file) 
-
             local_df = self._evaluate_single_local(subject_file)
             local_df = local_df[[col for col in local_df.columns if 'percentage_error' in col or col=='Organ' or col=='PID']]
             local_df['MAPE'] = local_df.apply(lambda row: self._get_local_MAPE(row), axis=1)
-            # Select return columns of local metrics
-            if local_verbose:
-                local_cols = [col for col in local_df.columns if 'percentage_error' in col or col=='MAPE'] 
-            else:
-                local_cols = ['MAPE']
-            local_df = local_df.groupby('PID').agg('mean')[local_cols]
+            local_df = local_df.groupby('PID').agg('mean')[['MAPE']]
 
             eval_df = pd.merge(global_df, local_df, on=['PID'], how='inner')
             all_df = all_df.append(eval_df)
 
-        # Calculate scores and fix column order
-        all_df['SCORE'] = all_df.apply(lambda row: self._get_subject_SCORE(row), axis=1)
-        # all_df = all_df[['PID', 'NRMSE', 'PSNR', 'SSIM', 'MAPE', 'SCORE']]
-
-        if save_to_file:
+            all_df['SCORE'] = all_df.apply(lambda row: self._get_subject_SCORE(row), axis=1)
+            all_df = all_df[['PID', 'NRMSE', 'PSNR', 'SSIM', 'MAPE', 'SCORE']]
             all_df.to_csv(self.report_csv, index=False)
-        return all_df
